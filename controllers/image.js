ad79e98be6d06678e6d4dfd73974820e63e41d1b
joshi365 <sumit.joshi.365@gmail.com>
2019-07-19 22:30:16 +0530

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -11,7 +11,7 @@ const handleApiCall = (req, res) => {
    .then(data => {
    	res.json(data);
    })
-   .catch(err => res.status(400).json('unablke to work with API'))
+   .catch(err => res.status(400).json('unable to work with API'))
 }
 
 const handleImage = (req, res, db) => {
@@ -26,6 +26,7 @@ const handleImage = (req, res, db) => {
 }
 
   module.exports = {
-       handleImage,
-       handleApiCall
+      handleApiCall,
+       handleImageS
+       
     };
